3c8417615b4345d572a00a49aaced0b623455bae
Marvin Mango <marvinke2001@gmail.com>
2025-03-21 17:30:21 +0300

    Array Filter Method

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+// 1. findMatching
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // 2. fuzzyMatch
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  // 3. matchName
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
